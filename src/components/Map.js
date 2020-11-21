860ea677abd361a1a586d81eeff1844d0eb05acc
larry <pascu.laurentiu17@gmail.com>
2020-11-21 18:51:34 +0200

    refactoring hardcoded natural event

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,11 +3,14 @@ import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
 import LocationInfoBox from './LocationInfoBox'
 
+// define constants
+const NATURAL_EVENT_WILDFIRE = 8;
+
 const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
     const markers = eventData.map(ev => {
-        if(ev.categories[0].id === 8) {
+        if(ev.categories[0].id === NATURAL_EVENT_WILDFIRE) {
             return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
         return null
